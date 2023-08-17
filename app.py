27e44d49417b01e0a2748cda8e062c81ecd9ad8f
mayank-kush <shockwavekush2482@gmail.com>
2023-08-17 17:37:44 +0530

    first commit

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen as uReq
 import logging
 logging.basicConfig(filename="scrapper.log" , level=logging.INFO)
+import pymongo
+from pymongo.mongo_client import MongoClient
 
 app = Flask(__name__)
 
@@ -72,6 +74,17 @@ def index():
                           "Comment": custComment}
                 reviews.append(mydict)
             logging.info("log my final result {}".format(reviews))
+            
+
+            uri = "mongodb+srv://shockwavekush2482:pwskills@cluster0.oh2f68k.mongodb.net/?retryWrites=true&w=majority"
+
+            
+            client = MongoClient(uri)
+            db= client['review_scrap']
+            review_col = db['review_scrap_data'] 
+            review_col.insert_many(reviews)
+            
+            
             return render_template('result.html', reviews=reviews[0:(len(reviews)-1)])
         except Exception as e:
             logging.info(e)
